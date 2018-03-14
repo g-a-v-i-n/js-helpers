a3f9c9f93f2587d92ef4db83c3e5e456eeea3f2f
Gavin Atkinson <gvnatknsn@gmail.com>
2018-03-14 19:05:16 -0400

    more helpers

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function incrementInList(list, currentIndex) {
+//  Basically a Gist
+
+
+const incrementInList = (list, currentIndex) => {
   const listLength = list.length
   if (currentIndex + 1 < listLength) {
     return list[currentIndex + 1]
@@ -6,21 +9,21 @@ function incrementInList(list, currentIndex) {
   return false
 }
 
-function decrementInList(list, currentIndex) {
+const decrementInList = (list, currentIndex) => {
   if (currentIndex > 0) {
     return list[currentIndex - 1]
   }
   return false
 }
 
-function updateInObject(originalObject, key, val) {
+const updateInObject = (originalObject, key, val) => {
   return {
     ...originalObject,
     [key]: val
   }
 }
 
-function loadImage(src) {
+const loadImage = (src) => {
   return new Promise((resolve, reject) => {
     const img = new Image()
     img.onload = () => resolve(img)
@@ -29,10 +32,23 @@ function loadImage(src) {
   })
 }
 
-// maybe
-module.exports = {
-  incrementInList,
-  decrementInList,
-  updateInObject,
-  loadImage,
+const isObject = obj => obj && typeof obj === 'object'
+
+const hasKey = (obj, key) => key in obj
+
+const assignWithStringPath = (obj, prop, value) => {
+  if (typeof prop === 'string') {
+    prop = prop.split('.')
+  }
+  if (prop.length > 1) {
+    const key = prop.shift()
+    obj[key] = Object.prototype.toString.call(obj[key]) === "[object Object]"
+      ? obj[key]
+      : {}
+    assignWithStringPath(obj, prop, value)
+  } else obj[prop[0]] = value
+}
+
+const deepClone = (obj) => {
+  return JSON.parse(JSON.stringify(obj))
 }
