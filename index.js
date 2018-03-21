c1b58ed15af59062d0a2f1edc71796f554d3134b
Gavin Atkinson <gvnatknsn@gmail.com>
2018-03-20 21:01:04 -0400

    add some nice type checking funcs

--- index.js
+++ index.js
@@ -32,7 +32,27 @@ const loadImage = src => {
   })
 }
 
-const isObject = a => a && typeof a === 'object' && value.constructor === Object
+const isObject = a => a && typeof a === 'object' && a.constructor === Object
+
+const isString = a => typeof a === 'string' || a instanceof String
+
+const isNumber = a => typeof a === 'number' && isFinite(a)
+
+const isFunction = a => typeof a === 'function'
+
+const isNull = a => a === null
+
+const isUndefined = a => typeof a === 'undefined'
+
+const isBoolean = a => typeof a === 'boolean'
+
+const isRegExp = a => a && typeof a === 'object' && a.constructor === RegExp
+
+const isError = a => a instanceof Error && typeof a.message !== 'undefined'
+
+const isDate = a => a instanceof Date
+
+const isSymbol = a => typeof a === 'symbol'
 
 const hasKey = (obj, key) => key in obj
 
